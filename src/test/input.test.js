5568e3c0675c6b8bb4fdb0e470fb02dd2fbd74ac
pegasus-ma <hongyu.ma@hotmail.fr>
2020-04-27 19:58:32 +0200

    Jest

--- src/test/input.test.js
+++ src/test/input.test.js
@@ -5,12 +5,16 @@ import IndexationInput from "bucares/components/indexation/input";
 // import App from "bucares";
 import { connect } from 'react-redux'
 import store from 'bucares/store';
+import  ReactTestUtils  from  'react-dom/test-utils';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 function setup() {
 	const props = {
-		store: store
+		store: store,
+		changeInputUrl: jest.fn(),
+		changeInputWord: jest.fn(),
+		clickButtonCheck: jest.fn()
 	}
 
 	const enzymeWrapper = mount(<IndexationInput {...props} />)
@@ -30,6 +34,39 @@ describe('components', () => {
 			expect(enzymeWrapper.find('table').exists()).toBe(true)
 			expect(enzymeWrapper.find('input').exists()).toBe(true)
 			expect(enzymeWrapper.find('button').exists()).toBe(true)
+
+		});
+		it('should listen url change', () => {
+			const event = {
+				// preventDefault() {},
+				target: { value: 'the-value' }
+			};
+			const { props, enzymeWrapper } = setup();
+			//expect(props.changeInputUrl).toBeCalledWith('');
+			enzymeWrapper.find('input').first().simulate('change', event);
+			//expect(props.changeInputUrl).toBeCalledWith('the-value');
+
+		});
+		it('should listen word change', () => {
+			const event = {
+				// preventDefault() {},
+				target: { value: 'the-value' }
+			};
+			const { props, enzymeWrapper } = setup();
+			//expect(props.changeInputWord).toBeCalledWith('');
+			enzymeWrapper.find('input').at(1).simulate('change', event);
+			//expect(props.changeInputWord).toBeCalledWith('the-value');
+
+		})
+		;
+		it('should listen onClick', () => {
+			const event = {
+				// preventDefault() {},
+				target: { value: 'the-value' }
+			};
+			const { props, enzymeWrapper } = setup();
+			enzymeWrapper.find('button').simulate('click');
+			//expect(props.clickButtonCheck).toBeCalledWith();
 
 		})
 	})
