dac574ac316131b11175d6d9079b93c9905c6a48
pegasus-ma <hongyu.ma@hotmail.fr>
2020-05-30 18:54:15 +0200

    properties

--- src/main/java/com/pegasus/indexation/controller/AppController.java
+++ src/main/java/com/pegasus/indexation/controller/AppController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@PropertySource(value = "classpath:indexation.properties")
 public class AppController {
 
     private static final Logger logger = LoggerFactory.getLogger(AppController.class);
